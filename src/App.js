34a2bfd39a85196e71aaa82ebeb9cff30adab1b2
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2024-03-05 13:28:06 -0300

    Learn indexedDB

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
-import React from 'react'
+import Dexie from 'dexie'
+import { useLiveQuery } from 'dexie-react-hooks'
 import './App.css'
 
+const db = new Dexie('todoApp')
+db.version(1).stores({
+  todos: '++id,task,completed',
+  users: '++id,name',
+})
+
+// @ts-ignore
+const { todos, users } = db
+
 const App = () => {
+  const allItems = useLiveQuery(() => todos.toArray(), [])
+  const allUsers = useLiveQuery(() => users.toArray(), [])
+
+  /*
+    we can use the where method to filter the items:
+    - db.todos.where('completed').equals(true).toArray()
+
+    but booleans, null, undefined and Object are not valid keys in IndexedDb
+    so we can either use strings or numbers for the completed status
+    or in our case, use the Array.filter method to filter completed/not-completed items
+  */
+
+  // eslint-disable-next-line no-unused-vars
+  const completedItems = allItems?.filter((item) => item.completed === true)
+
+  const addTask = async (event) => {
+    event.preventDefault()
+    const taskField = document.querySelector('#taskInput')
+
+    await todos.add({
+      task: taskField['value'],
+      completed: false,
+    })
+    await users.add({ name: 'John Doe' })
+
+    taskField['value'] = ''
+  }
+
+  const deleteTask = async (id) => todos.delete(id)
+
+  const toggleStatus = async (id, event) => {
+    await todos.update(id, { completed: !!event.target.checked })
+  }
+
   return (
     <div className="container">
       <h3 className="teal-text center-align">Todo App</h3>
-      <form className="add-item-form">
-        <input
-          type="text"
-          className="itemField"
-          placeholder="What do you want to do today?"
-          required
-        />
+      <form className="add-item-form" onSubmit={addTask}>
+        <input type="text" id="taskInput" placeholder="What do you want to do today?" required />
         <button type="submit" className="waves-effect btn teal right">
           Add
         </button>
@@ -19,29 +58,31 @@ const App = () => {
 
       <div className="card white darken-1">
         <div className="card-content">
-          <div className="row">
-            <p className="col s10">
-              <label>
-                <input type="checkbox" checked className="checkbox-blue" />
-                <span className="black-tex strike-text">Call John Legend</span>
-              </label>
-            </p>
-            <i className="col s2 material-icons delete-button">delete</i>
-          </div>
+          {!allItems?.length && <p className="center-align">You've not added any task yet.</p>}
 
-          <div className="row">
-            <p className="col s10">
-              <label>
-                <input type="checkbox" className="checkbox-blue" />
-                <span className="black-tex">Do my laundry</span>
-              </label>
-            </p>
-            <i className="col s2 material-icons delete-button">delete</i>
-          </div>
+          {allItems?.map(({ id, task, completed }) => (
+            <div className="row" key={id}>
+              <p className="col s10">
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={completed}
+                    className="checkbox-blue"
+                    onChange={(event) => toggleStatus(id, event)}
+                  />
+                  <span className="black-text">{task}</span>
+                </label>
+              </p>
+              <i onClick={() => deleteTask(id)} className="col s2 material-icons delete-button">
+                delete
+              </i>
+            </div>
+          ))}
         </div>
       </div>
     </div>
   )
 }
+
 
 export default App
